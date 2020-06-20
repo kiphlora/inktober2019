f2634e21dc9f16a89f93b648e39596cc8d72d9eb
kiphlora <bmoran.bam@gmail.com>
2020-06-20 19:33:55 -0400

    minor fixes

--- gen_pages.py
+++ gen_pages.py
@@ -108,9 +108,9 @@ main_page = f'''
 <title>Inktober 2019</title>
 
 <link href="https://fonts.googleapis.com/css?family=Dosis:200" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="kiphlora_website_stylingcss/project-theme.css">
-<link rel="stylesheet" type="text/css" href="kiphlora_website_stylingcss/page-style.css">
-<link rel="stylesheet" type="text/css" href="kiphlora_website_stylingfont-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="kiphlora_website_styling/css/project-theme.css">
+<link rel="stylesheet" type="text/css" href="kiphlora_website_styling/css/page-style.css">
+<link rel="stylesheet" type="text/css" href="kiphlora_website_styling/font-awesome/css/font-awesome.min.css">
 
 <style>
 
